2fb478d65aea386152cca07afa34fcd6649aec78
Erika <erikacardon@gmail.com>
2022-01-27 20:29:43 -0800

    first commit

--- node_js_project/app.js
+++ node_js_project/app.js
@@ -1,25 +1,52 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
 
 const app = express();
 
-const errorController = require('./controllers/404')
-
-app.set('view engine', 'ejs'); // For EJS
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById('61f34ec723d1027ec33067c8')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3001);
+mongoose.connect('mongodb+srv://main:LvPHax56Xf0ZE4RS@cluster0.4u4dj.mongodb.net/shop?retryWrites=true&w=majority')
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Erika',
+          email: 'erika@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  }).catch(err => {
+    console.log(err);
+  });
