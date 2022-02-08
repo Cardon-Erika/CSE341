7ec834990ab74a54dfa774d4dc96e73bc6d040be
Erika <erikacardon@gmail.com>
2022-02-08 12:13:25 -0800

    remove api key

--- node_js_project/controllers/auth.js
+++ node_js_project/controllers/auth.js
@@ -1,12 +1,15 @@
+const crypto = require('crypto');
+
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 const User = require('../models/user');
 
-const transporter =  nodemailer.createTransport(sendgridTransport({
+const transporter = nodemailer.createTransport(sendgridTransport({
     auth: {
-        // api_key: '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'
+        // remove key completely before a push.
+        // api_key:
     }
 }));
 
@@ -41,30 +44,32 @@ exports.getSignup = (req, res, next) => {
 exports.postLogin = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
-    User.findOne({email: email})
+    User.findOne({
+            email: email
+        })
         .then(user => {
             if (!user) {
                 req.flash('error', 'Invalid email or password.');
                 return res.redirect('/login');
             }
             bcrypt
-            .compare(password, user.password)
-            .then(doMatch => {
-                if (doMatch) {
-                    req.session.isLoggedIn = true;
-                    req.session.user = user;
-                    return req.session.save(err => {
-                        console.log(err);
-                        res.redirect('/');
-                    });
-                }
-                req.flash('error', 'Invalid email or password.');
-                res.redirect('/login');
-            })
-            .catch(err => {
-                console.log(err);
-                res.redirect('/login');
-            });
+                .compare(password, user.password)
+                .then(doMatch => {
+                    if (doMatch) {
+                        req.session.isLoggedIn = true;
+                        req.session.user = user;
+                        return req.session.save(err => {
+                            console.log(err);
+                            res.redirect('/');
+                        });
+                    }
+                    req.flash('error', 'Invalid email or password.');
+                    res.redirect('/login');
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.redirect('/login');
+                });
         })
         .catch(err => console.log(err));
 };
@@ -73,38 +78,42 @@ exports.postSignup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
-    User.findOne({email: email})
+    User.findOne({
+            email: email
+        })
         .then(userDoc => {
             if (userDoc) {
                 req.flash('error', 'E-Mail exists already, please pick a different one.');
                 return res.redirect('/signup');
             }
             return bcrypt.hash(password, 12)
-            .then(hashedPassword => {
-                const user = new User({
-                    email: email,
-                    password: hashedPassword,
-                    cart: { items: [] }
+                .then(hashedPassword => {
+                    const user = new User({
+                        email: email,
+                        password: hashedPassword,
+                        cart: {
+                            items: []
+                        }
+                    });
+                    return user.save();
+                })
+                .then(result => {
+                    res.redirect('/login');
+                    return transporter.sendMail({
+                        to: email,
+                        from: 'car15077@byui.edu',
+                        subject: 'Signup succeeded!',
+                        html: '<h1>You successfully signed up!</h1>'
+                    });
+                })
+                .catch(err => {
+                    console.log(err);
                 });
-                return user.save();
-            })
-            .then(result => {
-                res.redirect('/login');
-                return transporter.sendMail({
-                    to: email,
-                    from: 'car15077@byui.edu',
-                    subject: 'Signup succeeded!',
-                    html: '<h1>You successfully signed up!</h1>'
-                });
-            })
-            .catch(err => {
-                console.log(err);
-            });
         })
         .catch(err => {
             console.log(err);
         });
-    };
+};
 
 exports.postLogout = (req, res, next) => {
     req.session.destroy(err => {
@@ -112,3 +121,54 @@ exports.postLogout = (req, res, next) => {
         res.redirect('/');
     });
 };
+
+exports.getReset = (req, res, next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0]
+    } else {
+        message = null;
+    }
+    res.render('auth/reset', {
+        path: '/reset',
+        pageTitle: 'Reset Password',
+        errorMessage: message
+    });
+}
+
+exports.postReset = (req, rex, next) => {
+    crypto.randomBytes(32, (err, buffer) => {
+        if (err) {
+            console.log(err);
+            return res.redirect('/reset');
+        }
+        const token = buffer.toString('hex');
+        User.findOne({
+                email: req.body.email
+            })
+            .then(user => {
+                if (!user) {
+                    req.flash('error', "No account with that email found.");
+                    return res.redirect('/reset');
+                }
+                user.resetToken = token;
+                user.resetTokenExpiration = Date.now(0) + 3600000;
+                return user.save();
+            })
+            .then(result => {
+                res.redirect('/');
+                transporter.sendMail({
+                    to: req.body.email,
+                    from: 'car15077@byui.edu',
+                    subject: 'Password reset',
+                    html: `
+                        <p>You requested a password reset.</p>
+                        <p> Click this <a href="http://localhost:5000/reset/${token}">link</a> to set a new password.</p>
+                    `
+                });
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    });
+}
